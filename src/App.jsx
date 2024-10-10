16c8612da2fbd51f8d62815ef9cd4445a75fb6b0
DD <liudi@gmail.com>
2024-10-09 23:25:15 -0400

    Use state

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import PropTypes from 'prop-types'; 
 import './App.css'
 
-function MyButton({title}) {
+function MyButton({title, count, onClick}) {
   return (
-    <button>{title}</button>
+    <button onClick={() => onClick((count) => count + 1)}>
+    {title} {count}</button>
   );
 }
 MyButton.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  count: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired
 }
 
 function App() {
@@ -17,7 +20,7 @@ function App() {
   return (
       <div>
       <h1>Welcome to my app</h1>
-        <MyButton title="I'm a butt"/>
+        <MyButton title="I'm a butt" count={count} onClick={setCount}/>
       </div>
   );
 }
