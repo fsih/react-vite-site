a9aeeaef4baca1df71e4523d03bfb1e27482b2d9
DD <liudi@gmail.com>
2024-10-09 23:17:32 -0400

    pass button title and use prop types

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import PropTypes from 'prop-types'; 
 import './App.css'
+
+function MyButton({title}) {
+  return (
+    <button>{title}</button>
+  );
+}
+MyButton.propTypes = {
+  title: PropTypes.string.isRequired
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,9 +17,7 @@ function App() {
   return (
       <div>
       <h1>Welcome to my app</h1>
-        <button>
-            I'm a button
-        </button>
+        <MyButton title="I'm a butt"/>
       </div>
   );
 }
